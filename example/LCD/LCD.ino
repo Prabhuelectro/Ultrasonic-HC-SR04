4dead207fdc4b0ce17143b693b4b0ad2d97cd656
Prabhu <prabhuelectro99@gmail.com>
2020-04-11 06:02:06 +0530

    Create LCD.ino

---
+++ example/LCD/LCD.ino
@@ -0,0 +1,20 @@
+#include <Ultrasonic.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
+
+Ultrasonic hc(9,8); // (Trig PIN,Echo PIN)
+
+void setup() 
+{
+	lcd.begin(16, 2);
+}
+
+void loop()
+{
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print(ultrasonic.Ranging(CM)); // CM or INC
+	lcd.print("cm");
+	delay(100);
+} 
